f8266418a6348e39e28f4725f98851b0493780c1
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-11-28 21:38:53 +0300

    pipeline

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
 
 from backend.controllers.handlers import Handlers
 from backend.dtos.backend_front import FrontDTO
 
+class ZipRequest(BaseModel):
+    some: str  # Укажите ожидаемые поля
+
 app = FastAPI()
 
-@app.get("/zip")
-async def zip(data):
+@app.post("/zip")
+async def zip(file: UploadFile = File(alias="some")):
     # buisness skinny logic
     
-    zip = FrontDTO.Requests.zip(data)
+    file_content = await file.read()
+    
+    decoded = file_content.decode("utf-8", errors="ignore")
+    
+    print(type(decoded))
+    # zip = FrontDTO.Requests.zip(data)
     
     # validate data +  zipParse + processing + go to mistral + return response
-    response_corrections = await Handlers.code_style_fix(zip)
+    # response_corrections = await Handlers.code_style_fix(zip)
     
-    dto = FrontDTO.Responses.zip(response_corrections)
-    return dto
+    # dto = FrontDTO.Responses.zip(response_corrections)
+    return {"message": "zip"}
