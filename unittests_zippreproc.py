f3754eb0ed80296248af77972ad0699fbfb642ef
Crocodilchik <vovazh03@gmail.com>
2024-11-28 17:32:59 +0300

    Функции упакованы в класс. Добавлены юнит тесты (которые вроде даже проходятся)

---
+++ unittests_zippreproc.py
@@ -0,0 +1,181 @@
+import unittest
+from zip_preproc import ZipPreproc
+
+#Тест работоспособности класса
+class TestZipPreproc(unittest.TestCase):
+
+    def setUp(self):
+        self.test_file_1 = ZipPreproc(r'python\2020.2-Anunbis-develop.zip')
+        self.test_file_2 = ZipPreproc(r'python\backend-master.zip')
+        self.test_file_3 = ZipPreproc(r'python\donna-backend-master.zip')
+        self.test_file_4 = ZipPreproc(r'python\final-year-project-backend-api-master.zip')
+        self.test_file_5 = ZipPreproc(r'python\Flask-PostgreSQL-API-Seed-master.zip')
+        self.test_file_6 = ZipPreproc(r'python\FlaskApiEcommerce-master.zip')
+        self.test_file_7 = ZipPreproc(r'python\gramps-web-api-master.zip')
+        self.test_file_8 = ZipPreproc(r'python\hackernews-api-main.zip')
+        self.test_file_9 = ZipPreproc(r'python\http-api-3.1.zip')
+        self.test_file_10 = ZipPreproc(r'python\luncher-api-master.zip')
+        self.test_file_11 = ZipPreproc(r'python\ml-flask-api-master.zip')
+        self.test_file_12 = ZipPreproc(r'python\RESTfulAPI-master 2.zip')
+    
+    def test_create_json(self):
+        
+        #1 Тест с Anunbis-develop.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\Anunbis-develop.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on Anunbis-develop.zip with {e}')
+
+        #2 Тест с backend-master.zip
+        try:   
+            self.test_file_1.create_json(jsonname=r'data_jsons\backend-master.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on backend-master.zip with {e}')
+        
+        #3 Тест с donna-backend-master.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\donna-backend-master.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on donna-backend-master.zip with {e}')
+        
+        #4 Тест с final-year-project-backend-api-master.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\final-year-project-backend-api-master.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on final-year-project-backend-api-master.zip with {e}')
+        
+        #5 Тест с Flask-PostgreSQL-API-Seed-master.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\Flask-PostgreSQL-API-Seed-master.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on Flask-PostgreSQL-API-Seed-master.zip with {e}')
+        
+        #6 Тест с FlaskApiEcommerce-master.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\FlaskApiEcommerce-master.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on Flask-PostgreSQL-API-Seed-master.zip with {e}')
+        
+        #7 Тест с gramps-web-api-master.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\gramps-web-api-master.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on gramps-web-api-master.zip with {e}')
+        
+        #8 Тест с hackernews-api-main.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\hackernews-api-main.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on hackernews-api-main.zip with {e}')
+        
+        #9 Тест с http-api.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\http-api.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on http-api.zip with {e}')
+        
+        #10 Тест с luncher-api-master.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\luncher-api-master.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on luncher-api-master.zip with {e}')
+        
+        #11 Тест с ml-flask-api-master.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\ml-flask-api-master.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on ml-flask-api-master.zip with {e}')
+
+        #12 Тест с RESTfulAPI-master.zip
+        try:
+            self.test_file_1.create_json(jsonname=r'data_jsons\RESTfulAPI-master 2.json')
+        except Exception as e:
+            self.fail(msg=f'Failed on RESTfulAPI-master {e}')
+    
+    def test_find_file_types(self):
+
+        #1 Тест с Anunbis-develop.zip
+        res_func = self.test_file_1.find_file_types()
+        res_true = set(['gitignore', 'Dockerfile', 'README', 'env', 
+                    'Makefile', 'py', 'md', 'mako', 'pycodestyle', 'yml', 
+                    'lock', 'png', 'dev', 'Procfile', 'html', 'sh', 'Pipfile', 
+                    'txt', 'LICENSE', 'ini', 'json'])
+        self.assertTrue(res_func == res_true, msg='Fail with Anunbis-develop.zip')
+
+        #2 Тест с backend-master.zip
+        res_func = self.test_file_2.find_file_types()
+        res_true = set(['mako', 'yml', 'ini', 'md', 'mk', 'pylintrc', 
+                       'py', 'Makefile', 'in', 'sh', 'toml', 'LICENSE', 
+                       'gitignore', 'README', 'cfg'])
+        self.assertTrue(res_func == res_true, msg='Fail with backend-master.zip')
+
+        #3 Тест с donna-backend-master.zip
+        res_func = self.test_file_3.find_file_types()
+        res_true = set(['LICENSE', 'txt', 'db', 'py', 'gitignore', 'png', 'Procfile', 'md'])
+        self.assertTrue(res_func == res_true, msg='Fail with donna-backend-master.zip')
+
+        #4 Тест с final-year-project-backend-api-master.zip
+        res_func = self.test_file_4.find_file_types()
+        res_true = set(['prod', 'mako', 'lock', 'postman_collection', 
+                       'sh', 'txt', 'dev', 'py', 'ini', 'xml', 'README', 
+                       'gitignore', 'md', 'conf', 'Pipfile', 'env_sample', 'iml'])
+        self.assertTrue(res_func == res_true, msg='Fail with final-year-project-backend-api-master.zip')
+
+        #5 Тест с Flask-PostgreSQL-API-Seed-master.zip
+        res_func = self.test_file_5.find_file_types()
+        res_true = set(['LICENSE', 'py', 'yml', 'ini', 'mako', 
+                       'txt', 'Dockerfile', 'Vagrantfile', 'gitignore', 'md', 'README'])
+        self.assertTrue(res_func == res_true, msg='Fail with Flask-PostgreSQL-API-Seed-master.zip')
+
+        #6 Тест с FlaskApiEcommerce-master.zip
+        res_func = self.test_file_6.find_file_types()
+        res_true = set(['iml', 'xml', 'png', 'gitignore', 'md', 'py', 'mako', 'README', 'ini', 'bat'])
+        self.assertTrue(res_func == res_true, msg='Fail with FlaskApiEcommerce-master.zip')
+
+        #7 Тест с gramps-web-api-master.zip
+        res_func = self.test_file_7.find_file_types()
+        res_true = set(['cfg', 'json', 'toml', 'iml', 'README', 
+                       'xml', 'md', 'ini', 'py', 'onnx', 'mako', 'yml', 
+                       'html', 'LICENSE', 'txt', 'flake8', 'gitignore', 
+                       'pylintrc', 'sh', 'yaml', 'Dockerfile', 'editorconfig', 
+                       'in', 'dockerignore'])
+        self.assertTrue(res_func == res_true, msg='Fail with gramps-web-api-master.zip')
+
+        #8 Тест с hackernews-api-main.zip
+        res_func = self.test_file_8.find_file_types()
+        res_true = set(['ini', 'README', 'test', 'mako', 
+                       'production', 'Makefile', 'yaml', 'txt', 'json', 
+                       'pylintrc', 'dockerignore', 'env', 'yml', 'conf', 'md', 
+                       'j2', 'Procfile', 'editorconfig', 'sh', 'LICENSE', 'Dockerfile', 
+                       'py', 'cfg', 'gitignore'])
+        self.assertTrue(res_func == res_true, msg='Fail with hackernews-api-main.zip')
+
+        #9 Тест с http-api.zip
+        res_func = self.test_file_9.find_file_types()
+        res_true = set(['whitesource', 'ini', 'pyi', 'typed', 
+                       'sh', 'gitattributes', 'html', 'toml', 
+                       'txt', 'yaml', 'Makefile', 'md', 'gitignore', 
+                       'py', 'json', 'yml', 'bat', 'flake8', 'LICENSE', 'rst'])
+        self.assertTrue(res_func == res_true, msg='Fail with http-api.zip')
+
+        #10 Тест с luncher-api-master.zip
+        res_func = self.test_file_10.find_file_types()
+        res_true = set(['py', 'yml', 'txt', 'md', 'ini', 'xml', 'iml', 'gitignore', 'Dockerfile'])
+        self.assertTrue(res_func == res_true, msg='Fail with luncher-api-master.zip')
+
+        #11 Тест с ml-flask-api-master.zip
+        res_func = self.test_file_11.find_file_types()
+        res_true = set(['iml', 'rst', 'gitignore', 'nojekyll', 
+                       'LICENSE', 'dockerignore', 'bat', 'cfg', 
+                       'json', 'md', 'txt', 'Dockerfile', 'yml', 'env', 
+                       'xml', 'py', 'Makefile'])
+        self.assertTrue(res_func == res_true, msg='Fail with ml-flask-api-master.zip')
+
+        #12 Тест с RESTfulAPI-master.zip
+        res_func = self.test_file_12.find_file_types()
+        res_true = set(['py', 'md', 'iml', 'xml', 'gitignore'])
+        self.assertTrue(res_func == res_true, msg='Fail with RESTfulAPI-master.zip')
+
+if __name__ == "__main__":
+    #Запуск юнит-тестов: python unittests_zippreproc.py -v
+    unittest.main()
--- zip_preproc.py
+++ zip_preproc.py
@@ -2,77 +2,90 @@ from zipfile import ZipFile
 import json
 import pprint
 
-#Функция для красивого вывода многоуровневого словаря
-#   data_json - словарь
-def display_json(data_json):
-    pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(data_json)
+#Класс для работы с запакованным архивом
+#Атрибуты класса:
+#   filename - имя зип архива
+#   archieve - обьект ZipFile (архив)
+#   filelist - список с путями до файлов
+#   Dict_path - словарь со структурой архива
+class ZipPreproc:
 
-#Функция для поиска всевозможных расширений в списке с файлами
-#   filelist - список, содержащий имена файлов
-#Вывод:
-#   множество всевозможных расширений
-def find_file_types(filelist):
+    #Инициализация класса
+    #   filename - имя архива
+    def __init__(self, filename):
+        self.filename = filename    #Имя файла
+        self.archieve = ZipFile(filename, mode='r') #Архив
+        self.filelist = []  #Список всез файлов
+        self.Dict_path = {}
 
-    st = set() #возвращаемое множество
+        for item in self.archieve.infolist():    #Создание списка со всеми файлами, кроме тех, что находятся в директории 'MACOSX'
+            fname = item.filename
 
-    for item in filelist:
-        end_s = list(item[-1].split('.'))[-1]
-        st.add(end_s)
-    return st
+            if fname[-1] != '/' and not('MACOSX' in fname) :
+                self.filelist.append(list(fname.split('/')))
+        
+        for item in self.filelist:   #Заполнение словаря
+            self.__create_dict(list_path=item)
 
-#Функция для создания словаря из списка, повторяющего структуру копируемого пути файла.
-#   archv - обьект ZipFile (архив) из которого читаются файлы
-#   dict - словарь, в который записывается информация
-#   list_path - список файла с путем к нему вида -['Директория1', 'Директория 2', ... 'файл']
-#   indx - индекс начального элемента
-#Вывод:
-#   словарь со структурой, повторяющей путь файла
-def create_dict(archv, dict, list_path, indx = 0):
+    #Функция для красивого вывода многоуровневого словаря
+    #   data_json - словарь
+    def display_json(self):
+        pp = pprint.PrettyPrinter(indent=2)
+        pp.pprint(self.Dict_path)
 
-    if indx < len(list_path) - 1:   #Если не дошли до самого файла, то углубляемся в следующую директорию
-        if not(list_path[indx] in dict.keys()):
-            dict[list_path[indx]] = {}
-        return create_dict(archv, dict[list_path[indx]], list_path, indx+1)
-    else:                           #Если дошли, то считываем файл в string. Игнорируем картинки png и все файлы из директории MAKOSX
-        names = archv.namelist()
-        for name in names:
-            if name.endswith(list_path[indx]) and not('MACOSX' in name):
-                with archv.open(name, 'r') as file:
-                    if not name.endswith('png'):
-                        dict[list_path[indx]] = file.read().decode('utf-8', errors='ignore')
-                    else:
-                        dict[list_path[indx]] ='some image'
-        return dict
+    #Функция для поиска всевозможных расширений в списке с файлами
+    #   filelist - список, содержащий имена файлов
+    #Вывод:
+    #   множество всевозможных расширений
+    def find_file_types(self):
 
-#Функция создания json непосредственно из архива zip
-#   filename - имя архива
-#   jsonname - имя json, в который будет сохраняться инфорация (если существует, то старая информация на нем будет стерта). По умолчанию 'PreprocData.json'
-#Результат работы:
-#   файл json, повторяющий структуру архива
-def create_json(filename, jsonname = 'PreprocData.json'):
+        st = set() #возвращаемое множество
 
-    #Чтение архива и создание всех необходимых переменных
-    archieve = ZipFile(filename, mode='r')
-    filelist = []
-    Dict_path = {}
+        for item in self.filelist:
+            end_s = list(item[-1].split('.'))[-1]
+            st.add(end_s)
+        return st
 
-    for item in archieve.infolist():    #Создание списка со всеми файлами, кроме тех, что находятся в директории 'MACOSX'
-        fname = item.filename
+    #Функция для создания словаря из списка, повторяющего структуру копируемого пути файла.
+    #   archv - обьект ZipFile (архив) из которого читаются файлы
+    #   dict - словарь, в который записывается информация
+    #   list_path - список файла с путем к нему вида -['Директория1', 'Директория 2', ... 'файл']
+    #   indx - индекс начального элемента
+    #Вывод:
+    #   словарь со структурой, повторяющей путь файла
+    def __create_dict(self, list_path, indx = 0):
 
-        if fname[-1] != '/' and not('MACOSX' in fname) :
-            filelist.append(list(fname.split('/')))
+        if indx < len(list_path) - 1:   #Если не дошли до самого файла, то углубляемся в следующую директорию
+            if not(list_path[indx] in self.Dict_path.keys()):
+                self.Dict_path[list_path[indx]] = {}
+            return self.__create_dict(list_path, indx+1)
+        else:                           #Если дошли, то считываем файл в string. Игнорируем картинки png и все файлы из директории MAKOSX
+            names = self.archieve.namelist()
+            for name in names:
+                if name.endswith(list_path[indx]) and not('MACOSX' in name):
+                    with self.archieve.open(name, 'r') as file:
+                        if not name.endswith('png'):
+                            self.Dict_path[list_path[indx]] = file.read().decode('utf-8', errors='ignore')
+                        else:
+                            self.Dict_path[list_path[indx]] ='some image'
+            return self.Dict_path
 
-    for item in filelist:   #Заполнение словаря
-        create_dict(archieve, Dict_path, item)
+    #Функция создания json непосредственно из архива zip
+    #   filename - имя архива
+    #   jsonname - имя json, в который будет сохраняться инфорация (если существует, то старая информация на нем будет стерта). По умолчанию 'PreprocData.json'
+    #Результат работы:
+    #   файл json, повторяющий структуру архива
+    def create_json(self, jsonname = 'PreprocData.json'):
+
+        with open(jsonname, 'w') as file: #Запись словаря в файл json
+            json.dump(self.Dict_path, file)
 
 
-    with open(jsonname, 'w') as file: #Запись словаря в файл json
-        json.dump(Dict_path, file)
-
-
-    archieve.close() #Закрытие чтения архива
+    def __del__(self):
+        self.archieve.close()
 
 if __name__ =='__main__':
-    filename = '2020.2-Anunbis-develop.zip'
-    create_json(filename)
+    filename = r'python\RESTfulAPI-master 2.zip'
+    fil = ZipPreproc(filename)
+    st = fil.find_file_types()
+    print(st)
