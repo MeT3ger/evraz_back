21410e6a4a62adf5ebcd540e62ace2bc08b4cf51
Moshchenskii Mikhail <92210552+MeT3ger@users.noreply.github.com>
2024-11-30 09:48:57 +0300

    упростил работу с file_library

--- backend/helpers/file_library.py
+++ backend/helpers/file_library.py
@@ -28,54 +28,12 @@ class GetDataBase:
 
     def get_db():
         jsons = ''
-
-        with open(FileLibrary.get_json_path(Files.anubis), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-
-        with open(FileLibrary.get_json_path(Files.backend_master), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
         
-        with open(FileLibrary.get_json_path(Files.donna_backend_master), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.final_year), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.flask_postgres), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.flask_api), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.gramps_api), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.hacker_api), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.http_api), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.launcher_api), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.ml_flask_api), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-        
-        with open(FileLibrary.get_json_path(Files.rest_api), 'r') as file:
-            data = json.load(file)
-            jsons = jsons + '\n' + str(data)
-
+        for file in Files:
+            with open(FileLibrary.get_json_path(file), 'r') as file:
+                data = json.load(file)
+                jsons = jsons + '\n' + str(data)
+                
         return jsons
         
+print(GetDataBase.get_db())
